e295cec6e3f787e1d404609c878607049b49a722
arvindphogat <adphogat@gmail.com>
2026-05-17 15:39:53 +0800

    Add roboshop-bash and 01-frontend.sh

--- roboshop-bash/01-frontend.sh
+++ roboshop-bash/01-frontend.sh
@@ -7,6 +7,7 @@ echo "configuring the frontend component of the Roboshop application"
 #i want to run this script as root user
 ID=$(id -u)
 component="frontend"
+logfile="/tmp/${component}.log"
 
 if [ $ID -ne 0 ]; then
   echo "This script must be run as root or sudo user. Please run the script with sudo or as root."
@@ -16,26 +17,56 @@ fi
 
 
 echo "disabling the default nginx configuration"
-dnf module disable nginx -y
+dnf module disable nginx -y &>> $logfile
+
+if [ $? -eq 0 ]; then
+  echo "success"
+else
+  echo "failure"
+  exit 2
+fi
 
 echo "Enabling installing 24 version nginx"
-dnf module enable nginx:1.24 -y
-
+dnf module enable nginx:1.24 -y &>> $logfile
+if [ $? -eq 0 ]; then
+  echo "success"
+else
+  echo "failure"
+  exit 2
+fi
 echo "installing nginx"
-dnf install nginx -y
+dnf install nginx -y &>> $logfile
+if [ $? -eq 0 ]; then
+  echo "success"
+else
+  echo "failure"
+  exit 2
+fi
 
 echo "downloading the $component code"
 curl -L -o /tmp/$component.zip "https://stan-robotshop.s3.amazonaws.com/$component-v3.zip"
 
 echo "performing cleanup: removing old content from nginx html directory"
 cd /usr/share/nginx/html
-rm -rf *
+rm -rf * &>> $logfile
 
 echo "extracting the $component code"
-unzip -o /tmp/$component.zip -d /usr/share/nginx/html &>/dev/null
+unzip -o /tmp/$component.zip -d /usr/share/nginx/html &>> $logfile
+if [ $? -eq 0 ]; then
+  echo "success"
+else
+  echo "failure"
+  exit 2
+fi
 
 echo "starting $component nginx service"
-systemctl enable nginx
-systemctl start nginx
+systemctl enable nginx &>> $logfile
+systemctl start nginx &>> $logfile
+if [ $? -eq 0 ]; then
+  echo "success"
+else
+  echo "failure"
+  exit 2
+fi
 
 echo "$component component of the Roboshop application has been set up successfully"
