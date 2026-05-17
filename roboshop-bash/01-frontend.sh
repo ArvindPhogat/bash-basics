4f716c37b0601a1118b2b370959c01c0bdfd08db
arvindphogat <adphogat@gmail.com>
2026-05-17 15:04:54 +0800

    Add roboshop-bash and 01-frontend.sh

--- roboshop-bash/01-frontend.sh
+++ roboshop-bash/01-frontend.sh
@@ -4,6 +4,16 @@
 
 echo "configuring the frontend component of the Roboshop application"
 
+#i want to run this script as root user
+ID=$(id -u)
+
+if [ $ID -ne 0 ]; then
+  echo "This script must be run as root or sudo user. Please run the script with sudo or as root."
+  echo "ex: sudo bash $0 or # bash $0"
+  exit 1
+fi
+
+
 echo "disabling the default nginx configuration"
 dnf module disable nginx -y
 
